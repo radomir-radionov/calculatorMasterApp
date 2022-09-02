697753ea4fbf2c520759632dc8a324a675705e6e
YourRade <you@example.com>
2022-09-02 17:45:06 +0300

    init app

---
+++ src/pages/Home/index.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+import { PageLayout } from '@/layouts'
+
+import Loader from '@/components/Loader'
+
+import { Card, Heading } from './components'
+
+export default () => {
+  return (
+    <PageLayout>
+      <Card>1gggf</Card>
+    </PageLayout>
+  )
+}
